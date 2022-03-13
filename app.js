2541c7aec81fcd835b6bc69487031613ffcb4224
marcelo-gomj <marcelomgj005@gmail.com>
2022-03-13 19:07:43 -0300

    refactoring code #1

--- app.js
+++ app.js
@@ -2,51 +2,53 @@ function query(ele, all = true) {
     return all ? document.querySelectorAll(ele) : document.querySelector(ele); 
 }
 
-let numbers = [];
-
-function responsiveDisplay(display){
-    if(numbers.length > 13){
-        console.log(display.style.fontSize)
+function innerHtml(output, set = true){
+    if(set) return output.innerHTML
+    else return (digit, clear = false) => {
+        !clear ? output.innerHTML += digit : output.innerHTML = digit
     }
 }
 
-function addNumber(value){
-    if(numbers.length <= 15){
-        numbers.push(value);
-    }
-
-    if(numbers[0] === "0"){
-        numbers.pop(); 
-    }
+function getMemory(value){
+    value === undefined ? localStorage.setItem('value' , value) :
+    localStorage.getItem('value')
 }
 
-function inputDisplay(list){
-    const output = query('.output', false),
-    valHtml = list.join("");
-
-    responsiveDisplay(output);
-    query('.output', false).innerHTML = '';
-    if(list.length === 0){
-        output.innerHTML = 0;
+function displayItem(digit, value, setValue){
+    const strValue = String(value)
+    if(digit === ','){
+        if(!strValue.includes('.')) getMemory(value + '.')
+        console.log(getMemory());
+    }else if(value == '0'){
+        if(digit != '0') setValue('.', true);
     }else{
-        const valueFormated = Intl.NumberFormat({style: 'decimal'}).format(valHtml);
-        output.innerHTML = valueFormated;
+        localStorage.setItem('value', digit)
+    }  
+}
+
+function formatNumberFinal(valFinal){
+    if(valFinal.includes(',')){
+        return valFinal.split(',')
     }
+
+    return valFinal
 }
 
 function insertNumber(btns){
-    let inputEvt = 0;
-    btns.addEventListener('click', (evt) => {
-        addNumber(listnerNumber(evt));
-        inputDisplay(numbers);
-    })
+    const output = query('.output', false);
+    
+    btns.addEventListener('click', (event) => {
+        const value = getMemory();
+        const setValue = innerHtml(output, false);
+        const digit = innerHtml(event.target)
 
-    function listnerNumber(e) {
-        if (btns !== e.target) {
-            const num = e.target.innerHTML
-            return inputEvt = num;
-        }
-    }
+        displayItem(digit, value, setValue);
+
+        const formated = formatNumberFinal(value);
+        setValue(formated, true);
+    })
+    
 }
 
+getMemory(0);
 insertNumber(query('.btn-number', false));
